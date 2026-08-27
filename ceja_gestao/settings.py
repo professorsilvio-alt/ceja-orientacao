f1a2ea27b9a9df29b1c656499b005ffdba7b1e48
Professor Silvio <professorsilvio@local>
2026-08-27 19:33:14 -0300

    Configura servidor de e-mail Gmail SMTP oficial da escola (admcejamesquita@gmail.com) para envio de comprovantes de ponto

--- ceja_gestao/settings.py
+++ ceja_gestao/settings.py
@@ -135,17 +135,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ============================================================
 # E-MAIL (Gmail SMTP)
 # ============================================================
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'CEJA Profa Rosa Soares <noreply@ceja.edu.br>')
-
-if DEBUG and not EMAIL_HOST_USER:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'admcejamesquita@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'wbdhcawdjkwafdtk')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'CEJA Profa Rosa Soares <admcejamesquita@gmail.com>')
 
 BASE_URL = os.getenv('BASE_URL', 'http://127.0.0.1:8000')
 
