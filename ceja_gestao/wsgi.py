9651afc54a15cda0d52c9708efc5f7e51f044450
Professor Silvio <professorsilvio@local>
2026-09-11 14:15:52 -0300

    Adicionar auto-migrate automatico no wsgi.py

--- ceja_gestao/wsgi.py
+++ ceja_gestao/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ceja_gestao.settings')
 
 application = get_wsgi_application()
+
+# Executa migrações pendentes automaticamente ao iniciar/recarregar o servidor
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as _e:
+    print('Auto-migrate aviso:', _e, file=sys.stderr)
+
